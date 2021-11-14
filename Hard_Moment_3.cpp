bd6e41a65eb5bfbf5dc2785ad03c0c9c8b5ef17e
kondrov98 <78880721+kondrov98@users.noreply.github.com>
2021-11-14 18:59:06 +0300

    Add files via upload

--- Hard_Moment_3.cpp
+++ Hard_Moment_3.cpp
@@ -1,1 +1,81 @@
 #include <iostream>
+#include <list>
+#include <array>
+
+
+void arithmetic_mean(std::list<float>& l)
+	{
+	std::list<float>::iterator i;
+	float sum = 0;
+	int cursor = 0;
+	i = l.begin();
+	while (i != l.end())
+		  {
+		   sum += *i;
+		   cursor++;
+		  }
+	l.push_back(sum/cursor);
+	}
+
+
+class matrics_3x3
+{
+private:
+	std::array<int, 3> a;
+	std::array<int, 3> b;
+	std::array<int, 3> c;
+	std::array<std::array<int,3>, 3> d;
+public:
+	matrics_3x3(std::array<int, 3>a,
+			std::array<int, 3>b,
+			std::array<int, 3>c )
+			:a(a),b(b),c(c)
+	{
+		 
+	}
+	int matrix_determinant()
+	{
+		d = { a,
+			  b,
+			  c };
+			return d[0][0] * d[1][1] * d[2][2] + d[0][1] * d[1][2] * d[2][0] + d[1][0] * d[2][1] * d[0][2]-
+				  (d[0][2] * d[1][1] * d[2][0] + d[1][0] * d[0][1] * d[2][2] + d[0][0] * d[2][1] * d[1][2]);
+	}
+};
+
+
+
+class Iterator_for_range_based
+{
+private:
+	int* ptr;
+	int mass_ptr_now;
+	int V;
+
+public:
+	Iterator_for_range_based(int m_V) :V(m_V), mass_ptr_now(0)
+	{
+		ptr = new int[V];
+	}
+
+	void Plus_element(int r)
+	{
+		ptr[mass_ptr_now] = r;
+		mass_ptr_now++;
+	}
+
+	int* begin()
+	{
+		return &ptr[0];
+	}
+
+	int* end()
+	{
+		return &ptr[mass_ptr_now];
+	}
+
+	~Iterator_for_range_based()
+	{
+		delete[] ptr;
+	}
+};
