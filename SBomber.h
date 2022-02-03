de404d5f0a89cf8f8640db89f94a6dcd9cf4a28b
kondrov98 <78880721+kondrov98@users.noreply.github.com>
2022-02-03 19:11:44 +0300

    Домашняя работа №3

---
+++ SBomber.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <vector>
+
+#include "LevelGUI.h"
+#include "Plane.h"
+#include "Bomb.h"
+#include "Ground.h"
+#include "Tank.h"
+
+class SBomber
+{
+public:
+
+    SBomber();
+    ~SBomber();
+    
+    inline bool GetExitFlag() const { return exitFlag; }
+
+    void ProcessKBHit();
+    void TimeStart();
+    void TimeFinish();
+
+    void DrawFrame();
+    void MoveObjects();
+    void CheckObjects();
+
+private:
+
+    void CheckPlaneAndLevelGUI();
+    void CheckBombsAndGround();
+    void __fastcall CheckDestoyableObjects(Bomb* pBomb);
+
+    void __fastcall DeleteDynamicObj(DynamicObject * pBomb);
+    void __fastcall DeleteStaticObj(GameObject* pObj);
+
+    Ground * FindGround() const;
+    Plane * FindPlane() const;
+    LevelGUI * FindLevelGUI() const;
+    std::vector<DestroyableGroundObject*> FindDestoyableGroundObjects() const;
+    std::vector<Bomb*> FindAllBombs() const;
+
+    void DropBomb();
+
+    std::vector<DynamicObject*> vecDynamicObj;
+    std::vector<GameObject*> vecStaticObj;
+    
+    bool exitFlag;
+
+    uint64_t startTime, finishTime, passedTime;
+    uint16_t bombsNumber, deltaTime, fps;
+    int16_t score;
+};
+
+template < typename T>
+class Iterator
+{
+private:
+
+public:
+    virtual void begin() = 0;
+    virtual void end() = 0;
+};
+
+template < typename T>
+class BombIterator: public Iterator<T>
+{
+private:
+    T current;
+    vector <Bomb*>& ArrBomb;
+
+public:
+    virtual void begin()
+    {
+
+    }
+    virtual void end()
+    {
+
+    }
+};
